e846fd0fb58a8fab232e856db9e2cc8119593f81
Cody Farrey <codyfarrey@gmail.com>
2025-05-28 00:11:28 -0500

    parcel integration

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,6 @@
-import md from 'markdown-it';
+import markdownit from 'markdown-it';
+
+const md = markdownit()
 
 console.log("Parsing content...")
 const result = md.render("# this is a test\n---\n### \n```java\n\tSystem.out.println\n(\"with some java code\")\n```");
