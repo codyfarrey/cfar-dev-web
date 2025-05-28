97f9d2b87dfd489f513dc2cd2d5f45ac089c6c9a
Cody Farrey <codyfarrey@gmail.com>
2025-05-27 22:22:56 -0500

    some styling

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,8 @@
-console.log("Hello World!")
+import md from 'markdown-it';
+
+console.log("Parsing content...")
+const result = md.render("# this is a test\n---\n### \n```java\n\tSystem.out.println\n(\"with some java code\")\n```");
+console.log("result: " + result)
+
+
+
