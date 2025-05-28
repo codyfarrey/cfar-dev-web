29514a8c5e58ae8103de9c6cd56997fd006053f0
Cody Farrey <codyfarrey@gmail.com>
2025-05-28 00:35:03 -0500

    got it working

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,8 @@
 import markdownit from 'markdown-it';
 
-const md = markdownit()
+const md = markdownit();
+const fs = require('fs');
+
 
 console.log("Parsing content...")
 const result = md.render("# this is a test\n---\n### \n```java\n\tSystem.out.println\n(\"with some java code\")\n```");
