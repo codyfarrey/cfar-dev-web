7dd73cefd5877b5ce4f61aaddc53581b413e0591
Cody Farrey <codyfarrey@gmail.com>
2025-07-17 18:41:28 -0500

    add blog mock data and conditionally render posts based on array length

--- src/components/BlogSection.jsx
+++ src/components/BlogSection.jsx
@@ -1,17 +1,36 @@
+import React, { useEffect, useState } from 'react';
 import BlogPost from './BlogPost.jsx';
 
 import './BlogSection.css';
 
+const fetchBlogs = (data) => {
+    if (!data) {
+        return <div>Loading...</div>
+    }
+
+    return data.posts.map((blog, index) => 
+        <>
+            <BlogPost title={blog.title} date={blog.date} content={blog.content} />
+            {index < data.posts.length - 1 && <hr />}
+        </>
+    )
+};
+
 export default function BlogSection() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch('/blogs.json')
+            .then(response => response.json())
+            .then(jsonData => setData(jsonData))
+            .catch(error => console.error('Error fetching data:', error))
+    }, []);
+
     return(
     <div className='blog-section'>
         <div className='blog-card'>
             <h1>Blog</h1>
-            <BlogPost 
-                title="Welcome!" 
-                date="July 16th, 2025" 
-                content="Welcome to <cfar.dev/>! The page you are visiting right now is just my journal/blog. I post things here when I feel like it is something that is worth permanently documenting on the internet. No politics, no religion, only software, hardware, and finance related topics with a few life hacks here and there. I’ve also configured the web server so that if you do a raw curl (no arguments) to cfar.dev, you should get your IP address as a response. I use this regularly to check if I am actually connected to the internet through my VPN. I am also in the process of developing a few additional pages that could be useful if you are the type of person who likes to buy and sell stocks on the U.S. Stock Market or track your personal finances. I will post an update here and add a proper navigation bar when additional pages go live. Stay tuned!"
-            />
+            { fetchBlogs(data) }
         </div>
       </div>
     );
