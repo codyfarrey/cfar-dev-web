124f977279d2c67297c46260abab9cc6b6b6890c
Cody Farrey <codyfarrey@gmail.com>
2025-07-17 17:55:18 -0500

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+
+export default function Footer () {
+    return (
+        <div className='footer-container'>
+            <small>Copyright &copy; Cody Farrey</small>
+        </div>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import './index.css'
 import App from './App.jsx'
 
 import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbar />
     <App />
+    <Footer />
   </StrictMode>,
 )
